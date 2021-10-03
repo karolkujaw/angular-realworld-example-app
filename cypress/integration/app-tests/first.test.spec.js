b04d9d4532ee3303a94cc0569aa72a793fc2c828
Karol Kujawski <karol.kujawski@eu.equinix.com>
2021-10-03 11:24:01 +0200

    cosmetic changes

--- cypress/integration/app-tests/first.test.spec.js
+++ cypress/integration/app-tests/first.test.spec.js
@@ -8,12 +8,11 @@ describe('Our first test', () => {
     it('check the header of the initial page', () => {
         // cy.get('[data-cy=home_page_header]').should('have.text', 'conduit')
         cy.getByDataCy('home_page_header').should('have.text', 'conduit')
-
     })
 
-    const userCredentials = {"user":{"email":"kkujawski.t.a.@gmail.com","password":"CypressTSH"}}
+    const userCredentials = { "user": { "email": "kkujawski.t.a.@gmail.com", "password": "CypressTSH" } }
 
-    it.only('I can do the headless authorization', () => {
+    it('I can do the headless authorization', () => {
         cy.request('POST', 'https://api.realworld.io/api/users/login', userCredentials).its('body').then(body => {
             const token = body.user.token
             const username = body.user.username
@@ -28,5 +27,4 @@ describe('Our first test', () => {
             cy.get('h4').contains(username)
         })
     })
-
 })
